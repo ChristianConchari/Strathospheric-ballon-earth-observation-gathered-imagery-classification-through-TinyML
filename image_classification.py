4f842863fc2c982ba6c4b45f2eaf5130f27d395e
Christian Ricardo Conchari Cabrera <chrisconchari@gmail.com>
2022-10-27 10:49:22 -0400

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -4,35 +4,49 @@ import numpy as np
 import time
 
 # Cargar el modelo TFLite y ver algunos detalles sobre la entrada/salida
-TFLITE_MODEL = "/home/deepspace/Strathospheric-ballon-earth-observation-gathered-imagery-classification-through-TinyML/model_cnn_quant.tflite"
+TFLITE_MODEL = "/home/pi/deepspace/Strathospheric-ballon-earth-observation-gathered-imagery-classification-through-TinyML/model_cnn_quant.tflite"
 tflite_interpreter = Interpreter(model_path=TFLITE_MODEL)
 tflite_interpreter.allocate_tensors()
 
 input_details = tflite_interpreter.get_input_details()
 output_details = tflite_interpreter.get_output_details()
 
-image = cv2.imread("/home/deepspace/Buena_238.jpg")
-orig_h, orig_w, _ = image.shape
-cp_image = np.copy(image)
-image = cv2.resize(image, (100,100)).astype(np.float32)
-image = np.expand_dims(image, axis=0)
-tflite_interpreter.set_tensor(input_details[0]['index'], image)
+i=0
 
-tflite_interpreter.invoke()
+# Keep looping
+while True:
+    # Get the current time, increase delta and update the previous variable
+    delta += time() - previous
+    previous = time()
 
-start_time = time.time()
-tflite_model_predictions = tflite_interpreter.get_tensor(output_details[0]['index'])
-end_time = time.time()
+    # Check if 60 (or some other value) seconds passed
+    
+    if delta > 60:
+        i+=1
+        # Operations on image
+        # Create a new VideoCapture object
+        cap = cv2.VideoCapture(0)
+        _, img = cap.read()
+        cv2.imwrite(f"image{i}.jpg", img)
+        # Reset the time counter
+        delta = 0
+        cap.release()
 
-print(tflite_model_predictions)
+        orig_h, orig_w, _ = image.shape
+        cp_image = np.copy(image)
+        image = cv2.resize(image, (100,100)).astype(np.float32)
+        image = 1/255.0
+        image = np.expand_dims(image, axis=0)
+        tflite_interpreter.set_tensor(input_details[0]['index'], image)
 
-print(f'Image processed in {round((end_time-start_time)*1000,2)} ms')
+        tflite_interpreter.invoke()
+        tflite_model_predictions = tflite_interpreter.get_tensor(output_details[0]['index'])
 
-if tflite_model_predictions > 0:
-    pred = 'good'
-    cv2.rectangle(cp_image, (0,0), (orig_w, orig_h), (0,255,0), 3)
-else:
-    pred = 'bad'
-    cv2.rectangle(cp_image, (0,0), (orig_w, orig_h), (0,0,255), 3)
+        if tflite_model_predictions > 0.5:
+            pred = 'good'
+            cv2.rectangle(cp_image, (0,0), (orig_w, orig_h), (0,255,0), 3)
+        else:
+            pred = 'bad'
+            cv2.rectangle(cp_image, (0,0), (orig_w, orig_h), (0,0,255), 3)
 
-cv2.imwrite(f'prediction_{pred}.jpg',cp_image)
+        cv2.imwrite(f'prediction_{pred}.jpg', cp_image)
